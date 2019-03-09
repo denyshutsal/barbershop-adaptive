c411095bfc95009dd500b258bf02ccf81b6e5788
Denys Hutsal <denyshutsal@gmail.com>
2019-03-09 23:25:05 +0300

    оптимизировал svg картинки, настроил owl слайдер блока advantages, предварительно настроил адаптив с резиной блока advantages для мобильного и плншетного размеров

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,51 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+
+// Owl carousel settings (Advantages section)
+//----------------------------------------------------------------------
+$(document).ready(function() {
+  if ( $(window).width() < 768 ) {
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:50,
+      autoplay: true,
+      responsive:{
+        0:{
+          items:1
+        }
+      }
+    });
+  } else {
+    $('.advantages__slider').removeClass('owl-carousel owl-theme owl-loaded owl-drag');
+  }
+});
+
+$(window).resize(function() {
+  if ( $(window).width() < 768 ) {
+    startCarousel();
+  } else {
+    stopCarousel();
+  }
+});
+
+function startCarousel(){
+  $('.advantages__slider').addClass('owl-carousel owl-theme owl-loaded owl-drag');
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:50,
+    autoplay: true,
+    responsive:{
+      0:{
+        items:1
+      }
+    }
+  });
+}
+
+function stopCarousel() {
+  $('.owl-carousel').trigger('destroy.owl.carousel');
+  $('.advantages__slider').removeClass('owl-carousel owl-theme owl-loaded owl-drag');
+}
+
